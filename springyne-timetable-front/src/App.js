7e2759635140be43782f0ee0db4275bbb0ea6473
Egle Juraityte <egle.juraityte@stud.vtmc.lt>
2023-02-22 14:13:53 +0200

    fixed some frontend

--- springyne-timetable-front/src/App.js
+++ springyne-timetable-front/src/App.js
@@ -27,11 +27,9 @@ import CreateProgramPage from './pages/CreateProgram';
 import EditProgramPage from './pages/EditProgram';
 import ViewProgramPage from './pages/ViewProgram';
 
-import ViewGroupPage from './pages/ViewGroup';
-
-import CreateGroupPage from './pages/CreateGroup';
-// import EditGroupPage from './pages/EditGroup';
 // import ViewGroupPage from './pages/ViewGroup';
+import CreateGroupPage from './pages/CreateGroup';
+
 
 
 
@@ -52,7 +50,7 @@ function App() {
             <Route path="/subjects" element={<SubjectListPage />} />
             <Route path="/teachers" element={<TeacherListPage />} />
             <Route path="/groups" element={<GroupListPage />} />
-            <Route path="/groups/view/:id" element={<ViewGroupPage />} />
+            {/* <Route path="/groups/view/:id" element={<ViewGroupPage />} /> */}
             <Route path="/teachers/create" element={<CreateTeacherPage />} />
             <Route path="/teachers/edit/:id" element={<EditTeacherPage />} />
             <Route path="/teachers/view/:id" element={<ViewTeacherPage />} />
@@ -67,8 +65,8 @@ function App() {
             <Route path="/rooms/edit/:id" element={<EditRoomPage />} />
             <Route path="/programs" element={<ProgramListPage />} />
             <Route path="/groups/create" element={<CreateGroupPage />} />
-            {/* <Route path="/groups/edit/:id" element={<EditGroupPage />} />
-            <Route path="/groups/view/:id" element={<ViewGroupPage />} /> */}
+            {/* <Route path="/groups/edit/:id" element={<EditGroupPage />} /> */}
+            
 
           </Routes>
         </div>

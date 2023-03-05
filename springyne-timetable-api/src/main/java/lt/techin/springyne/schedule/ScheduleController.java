cd57084c76a9fd0a587881189cc15d6a22dd12b4
noea7 <114760692+noea7@users.noreply.github.com>
2023-03-05 14:22:55 +0200

    schedule list front end

--- springyne-timetable-api/src/main/java/lt/techin/springyne/schedule/ScheduleController.java
+++ springyne-timetable-api/src/main/java/lt/techin/springyne/schedule/ScheduleController.java
@@ -1,11 +1,14 @@
 package lt.techin.springyne.schedule;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/schedules")
@@ -27,5 +30,20 @@ public class ScheduleController {
     public ResponseEntity<Schedule> addSchedule(@Valid @RequestBody ScheduleDto scheduleDto,
                                                 @RequestParam Long groupId) {
         return ResponseEntity.ok(scheduleService.addSchedule(ScheduleMapper.toSchedule(scheduleDto), groupId));
+    }
+
+    @GetMapping("/{scheduleId}")
+    public Optional<Schedule> getScheduleById(@PathVariable Long scheduleId) {
+        return scheduleService.getScheduleById(scheduleId);
+    }
+
+    @GetMapping("/search")
+    public Page<Schedule> filterSchedulesByNameDatePaged(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) LocalDate date,
+            @RequestParam int page,
+            @RequestParam int pageSize) {
+
+        return scheduleService.searchByNameDatePaged(name, date, page, pageSize);
     }
 }

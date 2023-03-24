b25e2017c5571062206578797749d1361fde6270
Egle Juraityte <egle.juraityte@stud.vtmc.lt>
2023-03-24 09:41:36 +0200

    Room to PDF empty dates added

--- springyne-timetable-api/src/main/java/lt/techin/springyne/pdfExporter/RoomLessonPdfExporter.java
+++ springyne-timetable-api/src/main/java/lt/techin/springyne/pdfExporter/RoomLessonPdfExporter.java
@@ -8,11 +8,11 @@ import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import lt.techin.springyne.lesson.Lesson;
 import lt.techin.springyne.room.Room;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.List;
@@ -22,15 +22,19 @@ import java.util.Optional;
 public class RoomLessonPdfExporter {
     private List<Lesson> listLessons;
     private Optional<Room> room;
+    private String startDate;
+
+    private String endDate;
 
     private BaseFont baseFont = BaseFont.createFont("fonts/LiberationSans-Regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 
 
-    public RoomLessonPdfExporter(List<Lesson> listLessons, Optional<Room> room) throws IOException {
+    public RoomLessonPdfExporter(List<Lesson> listLessons, Optional<Room> room, String startDate, String endDate) throws IOException {
         this.listLessons = listLessons;
         this.room = room;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
-
 
 
     public void writeTableHeader(PdfPTable table) {
@@ -61,26 +65,50 @@ public class RoomLessonPdfExporter {
     private void writeTableData(PdfPTable table) {
         Locale lithuanian = new Locale("lt", "LT");
         Font font = new Font(baseFont, 12);
+        LocalDate sDate = LocalDate.parse(startDate);
+        LocalDate eDate = LocalDate.parse(endDate);
 
         LocalDate previousLessonDate = null;
+        LocalDate currentDate = sDate;
 
-        for (Lesson lesson : listLessons) {
-            LocalDate currentLessonDate = lesson.getLessonDate();
-
-            if (previousLessonDate == null || !previousLessonDate.equals(currentLessonDate)) {
-                table.addCell(new PdfPCell(new Phrase(String.valueOf(currentLessonDate), font)));
-                table.addCell(new PdfPCell(new Phrase(currentLessonDate.getDayOfWeek().getDisplayName(TextStyle.FULL, lithuanian), font)));
-                previousLessonDate = currentLessonDate;
-            } else {
-                table.addCell(new PdfPCell(new Phrase("", font)));
-                table.addCell(new PdfPCell(new Phrase("", font)));
+        while (!currentDate.isAfter(eDate)) {
+            if (currentDate.getDayOfWeek().equals(DayOfWeek.SATURDAY) || currentDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+                currentDate = currentDate.plusDays(1);
+                continue;
             }
 
-            // add other cell data for the lesson
-            table.addCell(new PdfPCell(new Phrase(String.valueOf(lesson.getLessonTime()), font)));
-            table.addCell(new PdfPCell(new Phrase(lesson.getTeacher().getName(), font)));
-            table.addCell(new PdfPCell(new Phrase(lesson.getSchedule().getGroup().getName(), font)));
+            boolean dateFound = false;
+            for (Lesson lesson : listLessons) {
+                LocalDate lessonDate = lesson.getLessonDate();
+
+                if (currentDate.equals(lessonDate)) {
+                    dateFound = true;
+                    if (previousLessonDate == null || !previousLessonDate.equals(currentDate)) {
+                        table.addCell(new PdfPCell(new Phrase(String.valueOf(lessonDate), font)));
+                        table.addCell(new PdfPCell(new Phrase(lessonDate.getDayOfWeek().getDisplayName(TextStyle.FULL, lithuanian), font)));
+                    } else {
+                        table.addCell(new PdfPCell(new Phrase("", font)));
+                        table.addCell(new PdfPCell(new Phrase("", font)));
+                    }
+                    table.addCell(new PdfPCell(new Phrase(String.valueOf(lesson.getLessonTime()), font)));
+                    table.addCell(new PdfPCell(new Phrase(lesson.getTeacher().getName(), font)));
+                    table.addCell(new PdfPCell(new Phrase(lesson.getSchedule().getGroup().getName(), font)));
+                    previousLessonDate = currentDate;
+                }
+            }
+
+            if (!dateFound) {
+                table.addCell(new PdfPCell(new Phrase(String.valueOf(currentDate), font)));
+                table.addCell(new PdfPCell(new Phrase(currentDate.getDayOfWeek().getDisplayName(TextStyle.FULL, lithuanian), font)));
+                table.addCell(new PdfPCell(new Phrase("", font)));
+                table.addCell(new PdfPCell(new Phrase("", font)));
+                table.addCell(new PdfPCell(new Phrase("", font)));
+                previousLessonDate = null;
+            }
+
+            currentDate = currentDate.plusDays(1);
         }
+
     }
 
     public void export(HttpServletResponse response) throws DocumentException, IOException {
@@ -98,7 +126,7 @@ public class RoomLessonPdfExporter {
 
         PdfPTable table = new PdfPTable(5);
         table.setWidthPercentage(100f);
-        table.setWidths(new float[] {2.0f, 3.0f,2.0f, 4.0f, 2.0f});
+        table.setWidths(new float[]{2.0f, 3.0f, 2.0f, 4.0f, 2.0f});
         table.setSpacingBefore(10);
 
         writeTableHeader(table);
